84e2d4197a32272f5743a0b50914600630030727
Holger Frey <mail@holgerfrey.de>
2018-02-28 11:39:25 +0100

    tests for syntax highlighting

--- pyragit/helper.py
+++ pyragit/helper.py
@@ -2,8 +2,7 @@ import mistune
 
 from mistune_contrib import highlight
 
-
-class HighlightRenderer(mistune.Renderer, highlight.HighlightMixin):
+class HighlightRenderer(highlight.HighlightMixin, mistune.Renderer):
     pass
 
 
@@ -16,9 +15,8 @@ def includeme(config):
     '''
 
     # make request.render_markdown() available for use in Pyramid
-    custom_renderer = HighlightRenderer()
-    render_markdown = mistune.Markdown(renderer=custom_renderer)
-    #render_markdown = mistune.Markdown()
+    renderer = HighlightRenderer(inlinestyles=False)
+    render_markdown = mistune.Markdown(renderer=renderer)
     config.add_request_method(
         lambda request, text: render_markdown(text), 
         'render_markdown'
