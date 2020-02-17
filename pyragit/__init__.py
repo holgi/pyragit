a3abf3bea599ed4f553aa94e1a749d31d9d64a37
Holger Frey <mail@holgerfrey.de>
2020-02-17 17:04:29 +0100

    Updated project setup to reflect https://github.com/holgi/cookiecutter_pyproject

--- pyragit/__init__.py
+++ pyragit/__init__.py
@@ -4,17 +4,19 @@ Simple rendering of markdown files in a git repository
 
 '''
 
+__version__ = "0.1.1"
+
 from pyramid.config import Configurator
 
 
 def main(global_config, **settings):
     ''' This function returns a Pyramid WSGI application. '''
-    
+
     config = Configurator(settings=settings)
     config.include('pyramid_jinja2')
     config.include('pyragit.resources')
     config.include('pyragit.markup')
     config.add_static_view('static', 'static', cache_max_age=3600)
     config.scan()
-        
+
     return config.make_wsgi_app()
